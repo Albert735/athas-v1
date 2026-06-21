0af5ab66d0bdd98f4cdacbb5eab7cb5ab2d51cb0
Albert735 <03jateee@gmail.com>
2026-06-21 02:18:52 +0000

    feat: implement active class card UI with navigation actions and schedule overview section

--- app/(tabs)/(schedule)/scheduled-class-list/index.tsx
+++ app/(tabs)/(schedule)/scheduled-class-list/index.tsx
@@ -1,5 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ActiveClassCard } from "@/components/timetable";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Badge } from "@/components/ui/badge";
+import { Dot } from "lucide-react-native";
 
 export default function ScheduledClassListScreen() {
   return (
@@ -10,6 +13,31 @@ export default function ScheduledClassListScreen() {
       </View>
 
       {/* active class card  */}
+
+      <View style={styles.activeClassCardContainer}>
+        <View style={styles.activeClassCardHeader}>
+          <Text>Ongoing Now</Text>
+          <Badge>
+            <View style={styles.badgeContainer}>
+              <Dot size={12} color="#fff" fill="#000" />
+              <Text style={styles.badgeText}>Live</Text>
+            </View>
+          </Badge>
+        </View>
+        <ActiveClassCard />
+      </View>
+
+      {/* upcomiing schedule  */}
+      <View>
+        <View style={styles.upcomingHeader}>
+          <Text style={styles.upcomingTitle}>Upcoming Schedule</Text>
+          <TouchableOpacity>
+            <Text>See All</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* single upcoming class card  */}
+      </View>
     </SafeAreaView>
   );
 }
@@ -17,8 +45,7 @@ export default function ScheduledClassListScreen() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    // backgroundColor: "#050505",
-    paddingHorizontal: 16,
+    paddingHorizontal: 20,
   },
   container: {
     flexDirection: "column",
@@ -38,5 +65,35 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "medium",
     color: "#fff",
+  },
+  activeClassCardContainer: {
+    marginTop: 20,
+  },
+  activeClassCardHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  badgeText: {
+    fontSize: 10,
+    fontWeight: "normal",
+    color: "#fff",
+  },
+  badgeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+  upcomingTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#111",
+  },
+  upcomingHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
   },
 });
--- components/timetable/active-class-card/active-class-card.tsx
+++ components/timetable/active-class-card/active-class-card.tsx
@@ -1,23 +1,72 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { DoorOpen, School, Navigation } from "lucide-react-native";
+import { Button } from "@/components/ui/button";
 
 export function ActiveClassCard() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Active Class</Text>
+      <Text style={styles.time}>9:30 AM - 11:20 AM</Text>
+
+      <Text style={styles.title}>Advanced Web Programming</Text>
+
+      <View style={styles.footer}>
+        <View style={styles.row}>
+          <DoorOpen size={18} color="#555" />
+          <Text style={styles.meta}>Room 2</Text>
+        </View>
+
+        <View style={styles.row}>
+          <School size={18} color="#555" />
+          <Text style={styles.meta}>NNB</Text>
+        </View>
+      </View>
+
+      <Button icon={Navigation} variant="default" size="sm">
+        Start Navigation
+      </Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#F4F4F4",
-    padding: 15,
-    borderRadius: 20,
-    gap: 8,
+    backgroundColor: "#F7F7F7",
+    padding: 16,
+    borderRadius: 16,
+    gap: 16,
   },
+
+  time: {
+    fontSize: 13,
+    color: "#666",
+  },
+
   title: {
     fontSize: 16,
-    fontWeight: "600",
+    fontWeight: "bold",
+    color: "#111",
+  },
+
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 4,
+  },
+
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 6,
+    backgroundColor: "#EDEDED",
+    width: "45%",
+    borderRadius: 10,
+    padding: 5,
+  },
+
+  meta: {
+    fontSize: 13,
+    color: "#555",
+    fontWeight: "bold",
   },
 });
