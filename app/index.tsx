b1d44e19d42ded28a8b1c86666e1beac6c55045e
Albert735 <03jateee@gmail.com>
2026-08-08 22:01:47 +0000

    chore: update index redirect to default to the home tab

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,9 @@ import { Redirect } from "expo-router";
 
 /**
  * Root Entry Redirect
- * 
+ *
  * Automatically redirects initial application route to main drawer tab structure.
  */
 export default function Index() {
-  return <Redirect href="/(drawer)" />;
+  return <Redirect href="/(drawer)/(tabs)/(home)" />;
 }
