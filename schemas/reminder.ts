4dee4b854664e8beb381440be7ef4280250cad44
Albert735 <03jateee@gmail.com>
2026-08-28 21:24:05 +0000

    refactor: update reminderSchema to include location coordinates, placeId, and mandatory date validation

--- schemas/reminder.ts
+++ schemas/reminder.ts
@@ -1,15 +1,21 @@
 import { z } from "zod";
 
-export const reminderSchema = z
-  .object({
-    note: z.string().min(1, "Please enter a reminder note"),
-    building: z.string().min(1, "Please select a location"),
-    dateTime: z.date().optional(),
-    alertNearby: z.boolean(),
-  })
-  .refine((data) => data.alertNearby || data.dateTime, {
-    message: "Set a date/time or enable 'Alert me when nearby'",
-    path: ["dateTime"],
-  });
+export const reminderSchema = z.object({
+  note: z.string().min(1, "Please enter a reminder note"),
+
+  placeId: z.string().min(1, "Please select a location"),
+
+  building: z.string().min(1, "Please select a location"),
+
+  buildingLatitude: z.number(),
+
+  buildingLongitude: z.number(),
+
+  dateTime: z.date({
+    message: "Please select a date and time",
+  }),
+
+  alertNearby: z.boolean(),
+});
 
 export type ReminderFormData = z.infer<typeof reminderSchema>;
