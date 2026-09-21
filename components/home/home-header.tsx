4ccd4167c5accbb33685fb21e386ac452193386b
Albert735 <03jateee@gmail.com>
2026-09-21 01:14:58 +0000

    feat:home-header image fix

--- components/home/home-header.tsx
+++ components/home/home-header.tsx
@@ -11,7 +11,7 @@ export function HomeHeader() {
   return (
     <View style={styles.header}>
       <Image
-        source={require("@/assets/images/raute-splash-icon-1024.png")}
+        source={require("@/assets/icon.png")}
         style={styles.logo}
         contentFit="contain"
       />
